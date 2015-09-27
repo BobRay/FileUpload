69ba5d8538ce5479b522daebfea0fbc23479bd49
bobray <bobray@softville.com>
2015-09-27 17:54:40 -0500

    Update comment

--- core/components/fileupload/elements/snippets/fileupload.snippet.php
+++ core/components/fileupload/elements/snippets/fileupload.snippet.php
@@ -32,7 +32,7 @@
  */
 // return include "c:/xampp/htdocs/addons/assets/mycomponents/fileupload/core/components/fileupload/elements/snippets/fileupload.snippet.php";
 /*
- * FileUpload snippet - Version 1.0.4
+
  * Original snippet by Michel van de Wetering
  * Refactored for MODx Revolution by Bob Ray
  *
