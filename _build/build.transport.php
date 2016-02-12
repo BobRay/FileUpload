c3493f07b0199c2bcc46b40d548b3916267566e0
bobray <bobray@softville.com>
2016-02-12 17:26:52 -0500

    Version to 1.2.0-pl -- released

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -30,7 +30,7 @@
 /* Set package info be sure to set all of these */
 define('PKG_NAME','FileUpload');
 define('PKG_NAME_LOWER','fileupload');
-define('PKG_VERSION','1.1.0');
+define('PKG_VERSION','1.2.0');
 define('PKG_RELEASE','pl');
 define('PKG_CATEGORY','FileUpload');
 
